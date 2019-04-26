13a518437ccd415b97349292b8c518adf74973e0
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-04-26 11:06:07 -0700

    Performed CRUD operations on projects and actions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 const server = require("./server.js");
 
-server.listen(4000, () => {
-  console.log("\n*** Server Running on http://localhost:4000 ***\n");
+server.listen(8000, () => {
+  console.log("\n*** Server Running on http://localhost:8000 ***\n");
 });
--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require("express");
 const helmet = require("helmet");
 const server = express();
 
+// middlewares
 server.use(express.json());
 server.use(helmet());
 
+const projectsRouter = require("./routers/projects-router");
+const actionsRouter = require("./routers/actions-router.js");
+
+// routing
+server.use("/api/projects", projectsRouter);
+server.use("/api/actions", actionsRouter);
+
+// route handlers
 server.get("/", (req, res) => {
   res.send("It's working!");
 });
